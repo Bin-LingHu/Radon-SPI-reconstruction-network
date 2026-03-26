4e0eb4cb1d5d5eb3d366af2163876b2cb7da8158
Bin-LingHu <binlinghu@mail.ustc.edu.cn>
2026-03-26 11:52:01 +0800

    Add files via upload

---
+++ models/bicycle_gan_model.py
@@ -0,0 +1,226 @@
+import torch
+from torchvision import transforms
+from .base_model import BaseModel
+from . import networks_cbn as networks
+import torchvision.models as models
+
+import importlib, os
+def init_id_model(opt):
+
+    model  = models.vgg19(pretrained=True).cuda()
+    return model
+
+class BiCycleGANModel(BaseModel):
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        return parser
+
+    def __init__(self, opt, id_model=None):
+        if opt.isTrain:
+            assert opt.batch_size % 2 == 0  # load two images at one time.
+
+        BaseModel.__init__(self, opt)
+        # specify the training losses you want to print out. The program will call base_model.get_current_losses
+        self.loss_names = ['G_GAN', 'D', 'G_L1']
+        # specify the images you want to save/display. The program will call base_model.get_current_visuals
+        self.visual_names = ['real_B_encoded', 'real_A_encoded',  'fake_B_encoded']
+        # self.visual_names = ['real_B_encoded', 'real_A_encoded', 'fake_B_random', 'fake_B_encoded']
+        # specify the models you want to save to the disk. The program will call base_model.save_networks and base_model.load_networks
+        use_D = opt.isTrain and opt.lambda_GAN > 0.0
+        use_D2 = opt.isTrain and opt.lambda_GAN2 > 0.0 and not opt.use_same_D
+        use_E = opt.isTrain or not opt.no_encode
+        use_vae = False #原始代码是True
+        self.model_names = ['G']
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.nz, opt.ngf, netG=opt.netG,
+                                      norm=opt.norm, nl=opt.nl, use_dropout=opt.use_dropout, init_type=opt.init_type, init_gain=opt.init_gain,
+                                      gpu_ids=self.gpu_ids, where_add=opt.where_add, upsample=opt.upsample)
+        D_output_nc = opt.input_nc + opt.output_nc if opt.conditional_D else opt.output_nc
+        if use_D:
+            self.model_names += ['D']
+            self.netD = networks.define_D(D_output_nc, opt.ndf, netD=opt.netD, norm=opt.norm, nl=opt.nl,
+                                          init_type=opt.init_type, init_gain=opt.init_gain, num_Ds=opt.num_Ds, gpu_ids=self.gpu_ids)
+        if use_D2:
+            self.model_names += ['D2']
+            self.netD2 = networks.define_D(D_output_nc, opt.ndf, netD=opt.netD2, norm=opt.norm, nl=opt.nl,
+                                           init_type=opt.init_type, init_gain=opt.init_gain, num_Ds=opt.num_Ds, gpu_ids=self.gpu_ids)
+        else:
+            self.netD2 = None
+        if use_E:
+            self.model_names += ['E']
+            self.netE = networks.define_E(opt.output_nc, opt.nz, opt.nef, netE=opt.netE, norm=opt.norm, nl=opt.nl,
+                                          init_type=opt.init_type, init_gain=opt.init_gain, gpu_ids=self.gpu_ids, vaeLike=use_vae)
+
+        if opt.isTrain:
+            self.criterionGAN = networks.GANLoss(gan_mode=opt.gan_mode).to(self.device)
+            self.criterionL1 = torch.nn.L1Loss()
+            self.criterionZ = torch.nn.L1Loss()
+            # initialize optimizers
+            self.optimizers = []
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G)
+            if use_E:
+                self.optimizer_E = torch.optim.Adam(self.netE.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+                self.optimizers.append(self.optimizer_E)
+
+            if use_D:
+                self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+                self.optimizers.append(self.optimizer_D)
+            if use_D2:
+                self.optimizer_D2 = torch.optim.Adam(self.netD2.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+                self.optimizers.append(self.optimizer_D2)
+
+        if id_model is not None:
+            print('id model is load !!!')
+        self.id_model = id_model
+
+    def is_train(self):
+        """check if the current batch is good for training."""
+        return self.opt.isTrain and self.real_A.size(0) == self.opt.batch_size
+
+    def set_input(self, input):
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def get_z_random(self, batch_size, nz, random_type='gauss'):
+        if random_type == 'uni':
+            z = torch.rand(batch_size, nz) * 2.0 - 1.0
+        elif random_type == 'gauss':
+            z = torch.randn(batch_size, nz)
+        return z.detach().to(self.device)
+
+    def encode(self, input_image):
+        z = self.netE.forward(input_image)
+        # std = logvar.mul(0.5).exp_()
+        # eps = self.get_z_random(std.size(0), std.size(1))
+        # z = eps.mul(std).add_(mu)
+        return z
+
+    def test(self, z0=None, encode=False):
+        with torch.no_grad():
+            z0= self.netE(self.real_A)
+            # if encode:  # use encoded z
+            #     z0, _ = self.netE(self.real_B)
+            # if z0 is None:
+            #     z0 = self.get_z_random(self.real_A.size(0), self.opt.nz)
+            self.fake_B = self.netG(self.real_A, z0)
+            return self.real_A, self.fake_B, self.real_B
+
+    def forward(self):
+        # get real images
+
+        half_size = self.opt.batch_size // 2
+        # A1, B1 for encoded; A2, B2 for random
+        self.real_A_encoded = self.real_A[0:half_size]
+        self.real_B_encoded = self.real_B[0:half_size]
+        self.real_A_random = self.real_A[half_size:]
+        self.real_B_random = self.real_B[half_size:]
+
+
+        self.z_encoded = self.encode(self.real_A_encoded)  #shape[1,8]
+        self.z_encodedB = self.encode(self.real_B_encoded)
+
+
+        # get random z
+        self.z_random = self.get_z_random(self.real_A_encoded.size(0), self.opt.nz)
+
+        # generate fake_B_encoded
+        self.fake_B_encoded = self.netG(self.real_A_encoded, self.z_encoded)
+
+        # generate fake_B_random
+        self.fake_B_random = self.netG(self.real_A_encoded, self.z_random)
+
+
+        self.fake_data_encoded = self.fake_B_encoded
+        self.fake_data_random = self.fake_B_random
+        self.real_data_encoded = self.real_B_encoded
+        self.real_data_random = self.real_B_random
+
+
+    def backward_D(self, netD, real, fake):
+        # Fake, stop backprop to the generator by detaching fake_B
+        pred_fake = netD(fake.detach())
+        # real
+        pred_real = netD(real)
+        loss_D_fake, _ = self.criterionGAN(pred_fake, False)
+        loss_D_real, _ = self.criterionGAN(pred_real, True)
+        # Combined loss
+        loss_D = loss_D_fake + loss_D_real
+        loss_D.backward()
+        return loss_D, [loss_D_fake, loss_D_real]
+
+    def backward_G_GAN(self, fake, netD=None, ll=0.0):
+        if ll > 0.0:
+            pred_fake = netD(fake)
+            loss_G_GAN, _ = self.criterionGAN(pred_fake, True)
+        else:
+            loss_G_GAN = 0
+        return loss_G_GAN * ll
+
+    def backward_EG(self):
+        # 1, G(A) should fool D
+
+        self.loss_G_GAN = self.backward_G_GAN(self.fake_data_encoded, self.netD, self.opt.lambda_GAN)
+        
+        # 2, latent loss 
+        self.loss_latent = self.criterionL1(self.z_encodedB, self.z_encoded)
+
+        # 3, reconstruction |fake_B-real_B|
+        if self.opt.lambda_L1 > 0.0:
+            self.loss_G_L1 = self.criterionL1(self.fake_B_encoded, self.real_B_encoded) * self.opt.lambda_L1
+        else:
+            self.loss_G_L1 = 0.0
+
+        # 4, ID loss, use self.fake_B_encoded, self.real_B_encoded
+        if self.id_model is not None:
+            if self.opt.lambda_id > 0.0:
+
+                self.fake_B_encoded1 = transforms.functional.resize(self.fake_B_encoded, [224, 224])
+                fake_B_encode_feature = self.id_model(self.fake_B_encoded1.repeat(1,3,1,1))
+        
+                self.fake_B_random1 = transforms.functional.resize(self.fake_B_random, [224, 224])
+                fake_B_random_feature = self.id_model(self.fake_B_random1.repeat(1,3,1,1))
+                self.loss_id = (1.0 - torch.cosine_similarity(fake_B_encode_feature,
+                                                              fake_B_random_feature)) * self.opt.lambda_id
+            else:
+                self.loss_id = 0.0
+        else:
+            self.loss_id = 0.0
+
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1 + self.loss_latent + self.loss_id
+        self.loss_G.mean().backward(retain_graph=True)
+
+    def update_D(self):
+        self.set_requires_grad([self.netD, self.netD2], True)
+        # update D1
+        if self.opt.lambda_GAN > 0.0:
+            self.optimizer_D.zero_grad()
+            self.loss_D, self.losses_D = self.backward_D(self.netD, self.real_data_encoded, self.fake_data_encoded)
+
+            self.optimizer_D.step()
+
+    def backward_G_alone(self):
+        # 3, reconstruction |(E(G(A, z_random)))-z_random|
+        if self.opt.lambda_z > 0.0:
+            self.loss_z_L1 = self.criterionZ(self.mu2, self.z_random) * self.opt.lambda_z
+            self.loss_z_L1.backward()
+        else:
+            self.loss_z_L1 = 0.0
+
+    def update_G_and_E(self):
+        # update G and E
+        self.set_requires_grad([self.netD, self.netD2], False)
+        self.optimizer_E.zero_grad()
+        self.optimizer_G.zero_grad()
+        self.backward_EG()
+
+
+        self.optimizer_E.step()
+        self.optimizer_G.step()
+
+    def optimize_parameters(self):
+        self.forward()
+        self.update_G_and_E()
+        self.update_D()
+
